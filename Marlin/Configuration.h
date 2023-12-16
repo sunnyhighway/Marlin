b4520d5c47734c8b526ddaf7d8bf6f11aa0498dc
Bart Hijwegen <bart@hijwegen.com>
2023-12-16 14:44:27 +0100

    enabled PRINTCOUNTER

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2555,7 +2555,7 @@
  *
  * View the current statistics with M78.
  */
-//#define PRINTCOUNTER
+#define PRINTCOUNTER
 #if ENABLED(PRINTCOUNTER)
   #define PRINTCOUNTER_SAVE_INTERVAL 60 // (minutes) EEPROM save interval during print. A value of 0 will save stats at end of print.
 #endif
